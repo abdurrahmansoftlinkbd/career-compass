7774492fddc13a174c0829155637b8ba362a60c8
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-25 15:38:31 +0600

    added footer component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
@@ -11,6 +12,9 @@ const MainLayout = () => {
       {/* dynamicSection */}
       <Outlet></Outlet>
       {/* footer */}
+      <footer>
+        <Footer></Footer>
+      </footer>
     </div>
   );
 };
