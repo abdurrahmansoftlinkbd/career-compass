dd4f77ea386df44396da8658b066da1f436b6576
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-25 10:48:04 +0600

    added raw navbar from daisyUi and fontFamily in indexCss

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,14 @@
+import Navbar from "../components/Navbar";
+
 const MainLayout = () => {
   return (
     <div>
+      {/* navbar */}
+      <nav>
+        <Navbar></Navbar>
+      </nav>
+      {/* banner */}
+
       <h2>MainLayout</h2>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "career-primary": "#2A73CC",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
