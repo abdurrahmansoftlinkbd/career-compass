5bafb34e3695c2696d5fd178085ce7ba702b04dd
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-26 07:57:09 +0600

    added service cards and serviceDetails component

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -3,19 +3,23 @@ import PropTypes from "prop-types";
 const ServiceCard = ({ service }) => {
   const { image, serviceName, category, pricing, counselor } = service;
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-[19rem] lg:w-[22rem] shadow-xl">
       <figure className="px-10 pt-10">
         <img src={image} alt="Shoes" className="rounded-xl" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
+        <h2 className="card-title text-career-primary">
           {serviceName}
           <div className="badge badge-accent">{category}</div>
         </h2>
-        <p>Price: {pricing}</p>
-        <p>Counselor: {counselor}</p>
+        <p>
+          <span className="font-semibold">Price:</span> {pricing}
+        </p>
+        <p>
+          <span className="font-semibold">Counselor:</span> {counselor}
+        </p>
         <div className="card-actions">
-          <button className="btn bg-career-primary text-white hover:bg-blue-400 hover:border-blue-400 uppercase">
+          <button className="btn bg-career-primary rounded-full text-white hover:bg-blue-400 hover:border-blue-400 uppercase">
             Learn More
           </button>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,10 @@ const Home = () => {
       </header>
       {/* services */}
       <section className="mt-24 container w-11/12 mx-auto font-poppins">
-        <h2 className="text-4xl font-bold uppercase mb-6 text-center">
+        <h2 className="text-4xl font-bold uppercase mb-6 text-center text-career-primary">
           Services
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6">
+        <div className="container mx-auto w-11/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 ">
           {services.map((service) => (
             <ServiceCard key={service.id} service={service}></ServiceCard>
           ))}
